d35442e36c6cd71ba55838b74687ab0d7b7ac004
Haiylu <Haiylu.Kibret@gmail.com>
2020-03-01 12:46:01 -0500

    added admin and models file

--- core/models.py
+++ core/models.py
@@ -3,7 +3,12 @@ from django.db import models
 # Create your models here.
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_length=30)
+    type_of_food = models.CharField(max_length=100)
+    exp_date = models.DateTimeField('expiration date')
 
     def __str__(self):
-        return self.name
+        return self.type_of_food
+
+class Category(models.Model):
+    vegi=models.ForeignKey(Item, on_delete=models.CASCADE)
+   # meat=models.Charfield(max_length=100)
